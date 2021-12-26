81fab11dcc7866a3b39e974fc0ce57a8306e6079
Daniel MS <danielmccannsayles@gmail.com>
2021-12-25 18:26:20 -0800

    cursor->pointer on nav

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -48,7 +48,7 @@ function NavigationBar(props){
     return(
 
         <div style={{height:window.innerHeight*(1.1/10), position:'sticky', position: '-webkit-sticky', top:'0', zIndex:'100', }} className="w-screen bg-black text-white sticky flex justify-center"  >
-            <p style={{ display:displayArrow}} className="text-center w-full hover:bg-gray-900 text-white" onClick={onClick}>
+            <p style={{ display:displayArrow}} className="text-center w-full hover:bg-gray-900 text-white cursor-pointer" onClick={onClick}>
             <svg xmlns="http://www.w3.org/2000/svg" width="100" height={window.innerHeight*(1.1/10)} fill="currentColor" className="mx-auto" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
             </svg>
